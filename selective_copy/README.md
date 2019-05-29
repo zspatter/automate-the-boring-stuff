5e9a1aedb89e19127bde2084b76a32958bd4fd7f
zspatter <localhost@zspatter.dev>
2019-05-29 17:05:22 -0400

    Adds Selective Copy

---
+++ selective_copy/README.md
@@ -0,0 +1,3 @@
+# Selective Copy
+
+This is a program that walks the given source path and looks for any files that match the given extension. All matches are moved to the given destination path. In the event that a file of that name already exists in the destintion, the script will increment the file's name until it is unique.
---
+++ selective_copy/selective_copy.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+
+import os
+import shutil
+
+
+def selective_copy(source, destination_path, extension):
+    directory = os.path.abspath(destination_path)
+
+    # if destination doesn't exist, it's created
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # walks entire tree from source directory
+    for root, _, files in os.walk(os.path.abspath(source)):
+        print(f'\nSearching in {root}')
+
+        # iterates over each file
+        for filename in files:
+            # if it matches desired extension
+            if filename.endswith(extension):
+                filepath = os.path.join(root, filename)
+                destination_path = directory
+
+                # skip if source of file is destination
+                if os.path.dirname(filepath) == directory:
+                    continue
+
+                # if file of this name exists in destination
+                if os.path.exists(os.path.join(destination_path, filename)):
+                    base, _ = os.path.splitext(filename)
+                    increment = 1
+                    check_name = os.path.join(directory, base + '_' + str(increment) + extension)
+
+                    # increment filename until name is unique
+                    while os.path.exists(check_name):
+                        check_name = os.path.join(directory, base + '_' + str(increment) + extension)
+                        increment += 1
+
+                    destination_path = check_name
+
+                # move file to destination
+                shutil.copy(filepath, destination_path)
+                print(f'\tCopied {os.path.basename(filepath)} to {os.path.abspath(destination_path)}')
+
+
+if __name__ == '__main__':
+    selective_copy('..', './matches/', '.txt')
