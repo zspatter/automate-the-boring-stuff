e73493daad73bc35de27b034225adceb3feecdf8
zspatter <localhost@zspatter.dev>
2019-05-29 09:21:43 -0400

    Adds unit test for Mad Libs

---
+++ mad_libs/test_mad_libs.py
@@ -0,0 +1,15 @@
+from mad_libs import mad_libs
+
+
+def test_mad_libs(capsys):
+    input_values = ['silly', 'chandelier', 'screamed', 'pickup truck']
+
+    def mock_input(s):
+        return input_values.pop(0)
+
+    mad_libs.input = mock_input
+    mad_libs.mad_libs('./mad_libs/sample_files/input.txt', './mad_libs/sample_files/output.txt')
+
+    with open('./mad_libs/sample_files/output.txt') as out:
+        result = out.read()
+        assert result == 'The silly panda walked to the chandelier and then screamed. A nearby chandelier was unaffected by these events.\n'
