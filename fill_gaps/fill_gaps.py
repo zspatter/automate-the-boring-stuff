cd940c6c39e62d4feac01b0d2138c9fa7e1e38c4
zspatter <localhost@zspatter.dev>
2019-05-30 19:32:29 -0400

    Adds docstring function definitions and fixes padding with renames

--- fill_gaps/fill_gaps.py
+++ fill_gaps/fill_gaps.py
@@ -6,10 +6,29 @@ import shutil
 
 
 def get_matching_files(root, regex):
+    """
+    Generates a sorted list of files that match the regex pattern at
+    root's destination
+
+    :param str root: absolute path of directory to search
+    :param Pattern regex: pattern of filename
+    :return: sorted list of matching filenames
+    """
     return sorted([file for file in os.listdir(root) if regex.match(file)])
 
 
 def fill_sequence_gaps(root, prefix, extension):
+    """
+    This function finds all file names that start with the passed
+    prefix, end with the passed extension, and include a number. All of
+    these matches are searched to ensure the increment sequentially
+    (no missing numbers). If not, the files are renamed with the expected
+    number to close the gap.
+
+    :param str root: path of directory to search
+    :param str prefix: text the filename begins with
+    :param str extension: desired extension
+    """
     # ensures path is absolute
     root = os.path.abspath(root)
 
@@ -18,23 +37,23 @@ def fill_sequence_gaps(root, prefix, extension):
         extension = extension[1:]
 
     # builds regex with given prefix and extension to find pattern matches
-    regex = re.compile('(^' + prefix + r')(\d+)(.*)?(\.' + extension + '$)')
+    regex = re.compile(f'(^{prefix})(\\d+)(.*)?(\\.{extension}$)')
     matches = get_matching_files(root, regex)
 
     # where sequence begins
     start = int(regex.search(matches[0]).group(2))
-    expected = start
+    expected_num = start
     max_length = len(regex.search(matches[-1]).group(2))
 
     for match in matches:
         match_object = regex.search(match)  # match object to extract regex groups
         sequence_num = int(match_object.group(2))  # number of current file
 
-        if sequence_num != expected:
+        if sequence_num != expected_num:
             # if file number isn't expected, build new filename
             filename = f"{prefix}" \
-                f"{'0' * (max_length - len(str(sequence_num)))}" \
-                f"{expected}" \
+                f"{'0' * (max_length - len(str(expected_num)))}" \
+                f"{expected_num}" \
                 f"{match_object.group(3)}" \
                 f"{match_object.group(4)}"
 
@@ -43,7 +62,7 @@ def fill_sequence_gaps(root, prefix, extension):
             print(f'Renaming {match} to {filename}')
 
         # increment expected count
-        expected += 1
+        expected_num += 1
 
 
 if __name__ == '__main__':
@@ -54,8 +73,8 @@ if __name__ == '__main__':
     os.makedirs(path)
 
     # creates files with only odd numbers spam00<x>.txt
-    for x in range(1, 10, 2):
-        with open(os.path.join(path, f'spam00{x}.txt'), 'w') as spam:
-            spam.write(f'spam00{x}')
+    for x in range(1, 150, 2):
+        with open(os.path.join(path, f'spam{x:03}.txt'), 'w') as spam:
+            spam.write(f'spam{x:03}')
 
     fill_sequence_gaps(path, 'spam', '.txt')
