6a7b41724c4c0be264861f1b6e714e08c9ac9f00
zspatter <localhost@zspatter.dev>
2019-05-30 10:50:17 -0400

    Adds Find Files by Size

---
+++ find_files_by_size/README.md
@@ -0,0 +1,7 @@
+# Find Files by Size
+
+It’s not uncommon for a few unneeded but humongous files or folders to take up the bulk of the space on your hard drive. If you’re trying to free up room on your computer, you’ll get the most bang for your buck by deleting the most massive of the unwanted files. But first you have to find them.
+
+Write a program that walks through a folder tree and searches for exceptionally large files or folders—say, ones that have a file size of more than 100MB. (Remember, to get a file’s size, you can use `os.path.getsize()` from the `os module`) Print these files with their absolute path to the screen.
+
+This scipt finds all files above a given number of bytes within a given directory.
---
+++ find_files_by_size/find_files_by_size.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+import os
+
+
+def find_large_files(path, threshold):
+    """
+    Finds all files whose size is greater than or equal to threshold and
+    prints their absolute path and size to the console
+
+    :param str path: path to directory to search
+    :param int threshold: size threshold in bytes
+    """
+    path = os.path.abspath(path)
+    print(f'Searching for contents above the size: {threshold / 1_000_000:,.3f} MB'
+          f'\n\tSearching {path}\n')
+
+    for content in os.listdir(path):
+        filepath = os.path.join(path, content)
+
+        if os.path.isdir(filepath):
+            size = get_dir_size(filepath)
+        else:
+            size = os.path.getsize(filepath)
+
+        if size >= threshold:
+            print(f'{filepath}: {size / 1_000_000:,.3f} MB')
+
+
+def get_dir_size(path):
+    """
+    Get's the cumulative size of a directory and all of it's contents
+    
+    :param str path: absolute path to directory 
+    :return: int cumulative size of directroy
+    """
+    size = 0
+    for root, _, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(root, file)
+            size += os.path.getsize(filepath)
+
+    return size
+
+
+if __name__ == '__main__':
+    find_large_files(os.path.join('..', '..', '..'), 1_000_000)
