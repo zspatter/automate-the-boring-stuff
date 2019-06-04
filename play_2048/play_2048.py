85ebd954e017da6ac93b202d79de25b2ddda86af
zspatter <localhost@zspatter.dev>
2019-06-04 14:29:30 -0400

    removes redundant while loop by creating conditional function aliases
    this allows for the same behavior while reducing code duplication

--- play_2048/play_2048.py
+++ play_2048/play_2048.py
@@ -6,8 +6,9 @@ from selenium.webdriver.common.keys import Keys
 
 def play_2048(random_input=False):
     """
-    This function launches and plays a game of 2048 with random inputs.
-    Once the game is over, the browser exits and the final score is displayed
+    This function launches and plays a game of 2048 with either seqential
+    (up, right, down, left) or random inputs. Once the game is over,
+    the browser exits and the final score is displayed
     """
     # loads 2048
     browser = webdriver.Chrome(executable_path='C:\\chromedriver.exe')
@@ -18,25 +19,27 @@ def play_2048(random_input=False):
     html_element = browser.find_element_by_css_selector('html')
 
     # possible input keys for game play
-    input_keys = (Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT)
+    input_keys = (Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT)
+    game_input = lambda x: x
 
-    if not random_input:
-        # loop while game isn't over
-        while game_status.text != 'Game over!':
-            html_element.send_keys(Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT)
-            game_status = browser.find_element_by_css_selector('.game-container p')
-    else:
-        while game_status.text != 'Game over!':
-            html_element.send_keys(random.choice(input_keys))
-            game_status = browser.find_element_by_css_selector('.game-container p')
+    # if input should be random, change game_input alias to call random.choice
+    if random_input:
+        game_input = random.choice
+
+    # while game isn't over, either give random or sequential input
+    while game_status.text != 'Game over!':
+        html_element.send_keys(game_input(input_keys))
+        game_status = browser.find_element_by_css_selector('.game-container p')
 
     # gather and print final score
     score = int(browser.find_element_by_css_selector('.score-container').text)
-    print(f"{'Randomized input:' if random_input else 'Non-randomized input:':21}"
+    print(f"{'Randomized' if random_input else 'Sequential':<10} input:"
           f"\tGame over - your score: {score:6,d}!")
     browser.quit()
 
 
 if __name__ == '__main__':
-    play_2048()
-    play_2048(random_input=True)
+    for _ in range(10):
+        play_2048()
+        play_2048(random_input=True)
+        print()
