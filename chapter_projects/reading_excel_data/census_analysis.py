254552446c11e83c0ae22e92de544c7b41a4096c
zspatter <localhost@zspatter.dev>
2019-06-04 20:29:04 -0400

    Reformats console output

--- chapter_projects/reading_excel_data/census_analysis.py
+++ chapter_projects/reading_excel_data/census_analysis.py
@@ -3,13 +3,15 @@ from census2010 import all_data
 total_tracts = 0
 total_population = 0
 
-print(f'Printing summary for Indiana in the following form: [county], IN: [tracts] [population]\n')
+print(f"Printing summary for Indiana in the following form:"
+      f"\n{'[county], IN:':>16} {'[tracts]'} {'[population]'}\n")
 
-for county in census2010.all_data['IN'].keys():
-    print(f"{county}, IN: {census2010.all_data['IN'][county]['tracts']}"
-          f"{census2010.all_data['IN'][county]['population']}")
-    total_tracts += census2010.all_data['IN'][county]['tracts']
-    total_population += census2010.all_data['IN'][county]['population']
+for county in all_data['IN'].keys():
+    print(f"{county + ', IN:':>16}\t{all_data['IN'][county]['tracts']:3,d} "
+          f"\t{all_data['IN'][county]['population']:7,d}")
+    total_tracts += all_data['IN'][county]['tracts']
+    total_population += all_data['IN'][county]['population']
 
-print(f'\nSummary of census data for Indiana:'
-      f'\tCensus tracts: {total_tracts}\tPopulation: {total_population}')
+print(f"\nSummary of census data for Indiana:"
+      f"\n\t{'Census tracts:':<14} {total_tracts:9,d}"
+      f"\n\t{'Population:':<14} {total_population:9,d}")
