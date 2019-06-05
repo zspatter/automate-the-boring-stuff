d41c6df1b9dae3fb8a2d8ee80cec7c64ecbc63e3
zspatter <localhost@zspatter.dev>
2019-06-05 19:50:09 -0400

    Adds two functions that insert rows using openpyxl functions

--- blank_row_inserter/blank_row_inserter.py
+++ blank_row_inserter/blank_row_inserter.py
@@ -1,6 +1,9 @@
+#! /usr/bin/env python3
+
 import sys
 
 import openpyxl
+from openpyxl.utils.cell import get_column_letter
 
 
 def insert_blank_rows(index: int, filename: str, offset: int = 1):
@@ -35,6 +38,51 @@ def insert_blank_rows(index: int, filename: str, offset: int = 1):
     print(f"Resulting file saved as 'result_{filename}'")
 
 
+def insert_via_move_range(index: int, filename: str, offset: int = 1):
+    """
+    Achieves the same result as insert_blank_rows() using openpyxl's
+    move_range() function.
+
+    The purpose of this exercise was to manipulate the data myself,
+    so the original implementation has been kept.
+
+    :param int index: row number where blank row starts
+    :param int offset: number of blank rows
+    :param str filename: name of file (including ext)
+    """
+    wb = openpyxl.load_workbook(filename=filename)
+    sheet = wb.active
+    print('Inserting blank rows...')
+
+    max_column = get_column_letter(sheet.max_column)
+    sheet.move_range(f"a{index}:{max_column}{sheet.max_row}", rows=offset, translate=True)
+
+    wb.save('result_' + filename)
+    print(f"Resulting file saved as 'result_{filename}'")
+
+
+def easy_insert(index: int, filename: str, offset: int = 1):
+    """
+    Achieves the same result as insert_blank_rows() using openpyxl's
+    intended insert_rows() function for moving rows.
+
+    The purpose of this exercise was to manipulate the data myself,
+    so the original implementation has been kept. However, this is the
+    most practical and elegant solution.
+
+    :param int index: row number where blank row starts
+    :param int offset: number of blank rows
+    :param str filename: name of file (including ext)
+    """
+    wb = openpyxl.load_workbook(filename=filename)
+    sheet = wb.active
+    print('Inserting blank rows...')
+
+    sheet.insert_rows(idx=index, amount=offset)
+    wb.save('result_' + filename)
+    print(f"Resulting file saved as 'result_{filename}'")
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print('Usage: blank_row_inserter.py [index] [offset] [filename]')
@@ -44,5 +92,3 @@ if __name__ == '__main__':
         file = sys.argv[3]
 
         insert_blank_rows(index=start, offset=blank_number, filename=file)
-
-    insert_blank_rows(5, 'multiplication_table.xlsx')
