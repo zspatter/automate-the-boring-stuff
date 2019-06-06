2eefd1e173619027d5ff124033487a45d4ff5b29
zspatter <localhost@zspatter.dev>
2019-06-06 11:19:30 -0400

    adds sample files

---
+++ text_to_spreadsheet/sample_files/generate_text_files.py
@@ -0,0 +1,6 @@
+import random
+
+for x in range(1, 6):
+    with open(f'sample_text{x}.txt', 'w') as text:
+        for y in range(1, random.randint(3, 15)):
+            text.write(f'Line {y}: sample line in sample_text{x}.txt\n')
