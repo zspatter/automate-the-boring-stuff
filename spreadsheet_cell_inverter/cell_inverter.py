124b2a202d9ba59b047c1031b049c372f378ea4a
zspatter <localhost@zspatter.dev>
2019-06-06 09:52:13 -0400

    Adds Spreadsheet Cell Inverter

---
+++ spreadsheet_cell_inverter/cell_inverter.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+import os
+
+import openpyxl
+
+
+def invert_cells(filename: str):
+    wb = openpyxl.load_workbook(filename=filename)
+    sheet = wb.active
+
+    print(f"Transposing the content of sheet: '{sheet.title}'")
+    inverted_sheet = wb.create_sheet(title=f'Inverted {sheet.title}', index=0)
+
+    for row in sheet.rows:
+        for cell in row:
+            row = cell.row
+            col = cell.column
+
+            inverted_sheet.cell(row=col, column=row).value = cell.value
+
+    wb.save(f'result_{filename}')
+    print(f"Resulting file saved as 'result_{filename}'")
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.join(os.path.abspath('.'), 'sample_files'))
+    invert_cells('example.xlsx')
