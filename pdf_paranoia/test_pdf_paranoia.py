685acdb0664d80fdac4714b1514980e3a00376a5
zspatter <localhost@zspatter.dev>
2019-06-07 17:12:51 -0400

    adds setup_directory() and revert_directory() helper functions

--- pdf_paranoia/test_pdf_paranoia.py
+++ pdf_paranoia/test_pdf_paranoia.py
@@ -1,6 +1,9 @@
+import os
 from os.path import join, abspath
 
 from pdf_paranoia import pdf_paranoia
+
+original_wd = abspath(os.getcwd())
 
 
 def test_encrypt_pdfs():
@@ -26,3 +29,14 @@ def test_pdf_reader_generator():
 
 def test_copy_pdf_pages():
     pass
+
+
+def setup_directory(path):
+    os.chdir(abspath(path))
+
+
+def revert_directory(remove_files):
+    for file in remove_files:
+        os.remove(file)
+
+    os.chdir(original_wd)
