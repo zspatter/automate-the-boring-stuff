72d079787df5db0a8d1580bf14f0fe31c8878979
zspatter <localhost@zspatter.dev>
2019-06-07 16:26:58 -0400

    adds `decrypt_pdfs()` and  `pdf_reader_generator()` for the common needs shared between encrypt/decrypt functions

--- pdf_paranoia/pdf_paranoia.py
+++ pdf_paranoia/pdf_paranoia.py
@@ -1,52 +1,78 @@
 import os
 import re
-from os.path import join, abspath, splitext, basename
+from os.path import join, abspath, splitext, basename, dirname
 
 import PyPDF2
 
-regex = re.compile(r'(?i)'              # case insensitive
-                   r'^(.*)'             # any number of characters
-                   r'(_encrypted)?'     # optional '_encrypted'
-                   r'(.pdf)$',          # ends with .pdf
-                   re.VERBOSE)
-
 
 def encrypt_pdfs(path, password):
-    path = abspath(path)
+    for filepath, pdf, reader in pdf_reader_generator(path):
+        if not reader.isEncrypted:
+            writer = copy_pdf_pages(reader)
+            writer.encrypt(password)
 
-    for root, _, files in os.walk(path):
-        pdfs = [file for file in files if file.lower().endswith('.pdf')]
+            base, ext = splitext(basename(filepath))
+            new_filepath = join(dirname(abspath(filepath)), f'{base}_encrypted{ext}')
 
-        for filename in pdfs:
-            filepath = join(abspath(root), filename)
-            pdf = open(filepath, 'rb')
-            reader = PyPDF2.PdfFileReader(pdf)
+            with open(new_filepath, 'wb') as output:
+                writer.write(output)
 
-            if not reader.isEncrypted:
-                writer = PyPDF2.PdfFileWriter()
-
-                for page_num in range(reader.numPages):
-                    writer.addPage(reader.getPage(page_num))
-
-                writer.encrypt(password)
-                base, ext = splitext(filename)
-                new_filepath = join(abspath(root), f'{base}_encrypted{ext}')
-
-                with open(new_filepath, 'wb') as output:
-                    writer.write(output)
-
-                pdf.close()
-                remove_unencrypted_pdf(new_filepath, filepath, password)
+            pdf.close()
+            remove_unencrypted_pdf(new_filepath, filepath, password)
 
 
 def remove_unencrypted_pdf(encrypted_path, unencrypted_path, password):
     reader = PyPDF2.PdfFileReader(open(encrypted_path, 'rb'))
-    if reader.isEncrypted and reader.decrypt(password) == 1:
+    if reader.isEncrypted and reader.decrypt(password):
         os.remove(unencrypted_path)
     else:
         print(f"'{basename(encrypted_path)}' failed the encryption check, "
               f"so '{basename(unencrypted_path)}' was not deleted.")
 
 
+def decrypt_pdfs(path, password):
+    regex = re.compile(r'(?i)'              # case insensitive
+                       r'^(.*?)'            # any number of characters (non-greedy)
+                       r'(_encrypted)?'     # optional '_encrypted'
+                       r'(.pdf)$',          # ends with .pdf
+                       re.VERBOSE)
+
+    for filepath, pdf, reader in pdf_reader_generator(path):
+        if reader.isEncrypted:
+            if reader.decrypt(password):
+                writer = copy_pdf_pages(reader)
+
+                match_object = regex.search(basename(filepath))
+                new_filepath = join(dirname(abspath(filepath)),
+                                    f'{match_object.group(1)}{match_object.group(3)}')
+
+                with open(new_filepath, 'wb') as output:
+                    writer.write(output)
+                pdf.close()
+
+            else:
+                print(f"Decryption failed: wrong password provided for '{basename(filepath)}'")
+
+
+def pdf_reader_generator(path):
+    for root, _, files in os.walk(abspath(path)):
+        pdfs = [file for file in files if file.lower().endswith('.pdf')]
+
+        for filename in pdfs:
+            filepath = join(abspath(root), filename)
+            pdf = open(filepath, 'rb')
+            yield filepath, pdf, PyPDF2.PdfFileReader(pdf)
+
+
+def copy_pdf_pages(pdf_reader):
+    writer = PyPDF2.PdfFileWriter()
+
+    for page_num in range(pdf_reader.numPages):
+        writer.addPage(pdf_reader.getPage(page_num))
+
+    return writer
+
+
 if __name__ == '__main__':
     encrypt_pdfs('.', 'password')
+    decrypt_pdfs('.', 'password')
