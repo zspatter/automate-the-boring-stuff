527b7e5437b9f2b36d35190f878042db7a196c5c
zspatter <localhost@zspatter.dev>
2019-06-20 10:55:13 -0400

    adds Brute-Force PDF Password Breaker

---
+++ pdf_password_breaker/password_breaker.py
@@ -0,0 +1,35 @@
+from os.path import abspath, basename, join
+
+import PyPDF2
+
+
+def break_password(encrypted_pdf, pw_list):
+    pdf = PyPDF2.PdfFileReader(open(abspath(encrypted_pdf), 'rb'))
+    print(f"Attempting to decrypt '{basename(encrypted_pdf)}' via brute force")
+
+    with open(abspath(pw_list), 'r') as reader:
+        passwords = reader.read().split('\n')
+
+    for password in passwords:
+        if pdf.decrypt(password.lower()):
+            return password.lower()
+        elif pdf.decrypt(password.upper()):
+            return password.upper()
+
+
+if __name__ == '__main__':
+    # gathers absolute paths of the files
+    pdf_path = abspath(join('.', 'sample_files', 'encrypted_pdf.pdf'))
+    passwords_path = abspath(join('.', 'sample_files', 'dictionary.txt'))
+
+    # generates an empty, encrypted PDF
+    writer = PyPDF2.PdfFileWriter()
+    writer.encrypt('abnormality')
+    with open(file=pdf_path, mode='wb') as pdf_file:
+        writer.write(pdf_file)
+
+    # attempts to decrypt the file and prints the results
+    result = break_password(encrypted_pdf=pdf_path, pw_list=passwords_path)
+    output = f"\nSuccess!\nThe password is: '{result}'" if result else \
+        f"\nFailure!\nFailed to decrypt the pdf"
+    print(output)
