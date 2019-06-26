5bf1471287f1db3a4bb996510830b2e82ab162fa
zspatter <localhost@zspatter.dev>
2019-06-26 11:34:19 -0400

    adjust format of table for stopwatch output

--- chapter_projects/super_stopwatch/stopwatch.py
+++ chapter_projects/super_stopwatch/stopwatch.py
@@ -16,26 +16,29 @@ def stopwatch():
     previous_time = start
     lap_num = 1
     spacer = '  |  '
+    seperator = f"+{'-' * 14}+{'-' * 20}+{'-' * 18}+"
 
     # prints table header
-    print(f"{'-' * 55}"
-          f"\n|{'Lap #':^12} | "
+    print(f"+{'-' * 54}+"
+          f"\n|{'Super Stopwatch':^54}|"
+          f"\n{seperator}"
+          f"\n|{'Lap Number':^14}| "
           f"{'Total time':^19}| "
-          f"{'Lap time':^17}|"
-          f"\n{'-' * 55}", end='')
+          f"{'Lap Time':^17}|"
+          f"\n{seperator}", end='')
 
     try:
         while True:
             input()
             lap_time = f'{time.time() - previous_time:,.2f} s'
             total_time = f'{time.time() - start:,.2f} s'
-            print(f"|  Lap #{lap_num:<4}{spacer}"
+            print(f"|  Lap #{lap_num:<5}{spacer}"
                   f"{total_time:>16}{spacer}"
                   f"{lap_time:>14}  |", end='')
             lap_num += 1
             previous_time = time.time()
     except KeyboardInterrupt:
-        print(f"\n{'-' * 55}"
+        print(f"\n{seperator}"
               f"\n\nDone.")
 
 
