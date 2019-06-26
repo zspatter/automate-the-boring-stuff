b7bb0f9059766ee684d6c0209c7322c44f027813
zspatter <localhost@zspatter.dev>
2019-06-26 11:59:59 -0400

    adds inline commnets to explain the various print statement within the unicode table

--- chapter_projects/super_stopwatch/stopwatch.py
+++ chapter_projects/super_stopwatch/stopwatch.py
@@ -6,43 +6,47 @@ import time
 
 def print_instructions():
     input("Press ENTER to begin."
-          "\nAfterwards, press enter to 'click' the stopwatch."
+          "\nAfterwards, press ENTER to 'click' the stopwatch."
           "\nPress Ctrl + C to quit.\n")
     print('Starting.\n')
-
-
-def table_seperator(begin='╠', middle='╬', end='╣'):
-    return f"{begin}{'═' * 14}{middle}{'═' * 20}{middle}{'═' * 18}{end}"
 
 
 def stopwatch():
     start = time.time()
     previous_time = start
     lap_num = 1
-    spacer = '  ║  '
+    column_spacer = '  ║  '
 
     # print table header
-    print(f"{table_seperator(begin='╔', middle='═', end='╗')}"
+    print(f"{build_table_separator(outer_left='╔', inner='═', outer_right='╗')}"
           f"\n║{'Super Stopwatch':^54}║"
-          f"\n{table_seperator(middle='╦')}"
+          f"\n{build_table_separator(inner='╦')}"
           f"\n║{'Lap Number':^14}║ "
           f"{'Total time':^19}║ "
           f"{'Lap Time':^17}║"
-          f"\n{table_seperator()}", end='')
+          f"\n{build_table_separator()}", end='')
 
     try:
         while True:
             input()
             lap_time = f'{time.time() - previous_time:,.2f} s'
             total_time = f'{time.time() - start:,.2f} s'
-            print(f"║  Lap #{lap_num:<5}{spacer}"
-                  f"{total_time:>16}{spacer}"
+
+            # prints results of current stopwatch
+            print(f"║  Lap #{lap_num:<5}{column_spacer}"
+                  f"{total_time:>16}{column_spacer}"
                   f"{lap_time:>14}  ║", end='')
+
             lap_num += 1
             previous_time = time.time()
     except KeyboardInterrupt:
-        print(f"\n╚{'═' * 14}╩{'═' * 20}╩{'═' * 18}╝"
+        # closes the stopwatch table
+        print(f"\n{build_table_separator(outer_left='╚', inner='╩', outer_right='╝')}"
               f"\n\nDone.")
+
+
+def build_table_separator(outer_left='╠', inner='╬', outer_right='╣'):
+    return f"{outer_left}{'═' * 14}{inner}{'═' * 20}{inner}{'═' * 18}{outer_right}"
 
 
 if __name__ == '__main__':
