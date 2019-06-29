3321e4e3cf0e04b68a36443342a7dbdf638d111d
zspatter <localhost@zspatter.dev>
2019-06-28 22:32:36 -0400

    Adjusts the print statements to clear the screen properly

--- chapter_projects/simple_countdown/countdown.py
+++ chapter_projects/simple_countdown/countdown.py
@@ -7,12 +7,13 @@ import subprocess
 
 def countdown(seconds=60):
     for x in range(seconds, 0, -1):
-        print(f'\r{x} seconds', end='')
+        print(f'{reset_line}{x} seconds', end='', flush=True)
         time.sleep(1)
 
-    print(f'\rAlarm')
-    subprocess.Popen(['start', 'alarm.wav'], shell=True)
+    print(f'{reset_line}Alarm!')
+    subprocess.Popen(['open', 'alarm.wav'])
 
 
 if __name__ == '__main__':
-    countdown()
+    reset_line = '\u001b[2K\u001b[20D'
+    countdown(15)
