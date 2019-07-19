a710b29fd4bbaff4178a09fcffb5245a1dcb7e26
zspatter <localhost@zspatter.dev>
2019-07-18 21:16:28 -0400

    Renames directory to text_notifications

--- chapter_projects/text_notifications/notifications.py
+++ chapter_projects/text_notifications/notifications.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# notifications.py - defines notifications() that texts a message passed as string
+# notifications.py - defines text_myself() that texts a message passed as string
 from os import environ
 
 from twilio.rest import Client
