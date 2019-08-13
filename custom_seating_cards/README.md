7977a5d18852208786cff0c65bc9386eca1a9cf1
zspatter <localhost@zspatter.dev>
2019-08-13 11:48:43 -0400

    adds Custom Seating Cards

---
+++ custom_seating_cards/README.md
@@ -0,0 +1,5 @@
+# Custom Seating Cards
+
+[Chapter 13's Custom Invitations as Word Documents](../custom_invitations/)  included a practice project to create custom invitations from a list of guests in a plaintext file. As an additional project, use the `pillow` module to create images for custom seating cards for your guests. For each of the guests listed in the guests.txt file, generate an image file with the guest name and some flowery decoration.
+
+To ensure that each seating card is the same size, add a black rectangle on the edges of the invitation image so that when the image is printed out, there will be a guideline for cutting. The PNG files that Pillow produces are set to 72 pixels per inch, so a 4×5-inch card would require a 288×360-pixel image.
---
+++ custom_seating_cards/custom_seating_card.py
@@ -0,0 +1,82 @@
+import random
+from pathlib import Path
+
+from PIL import Image, ImageChops, ImageDraw, ImageFilter, ImageFont
+
+
+def make_seating_cards(guest_list_path, directory):
+    output_path = Path(guest_list_path.parent / 'seating_cards')
+    output_path.mkdir(exist_ok=True)
+
+    images = get_images(directory=directory)
+    guests = guest_list_path.read_text().strip().split('\n')
+
+    for guest in guests:
+        # add flower background
+        flower = resize_image(filename=random.choice(images))
+        card = Image.new('RGBA', (360, 288), 'white')
+        card.paste(flower, (0, 0))
+
+        # adds exterior border
+        border = Image.new('RGBA', (364, 292), 'black')
+        border.paste(card, (2, 2))
+
+        # adds centered text and saves final image
+        card = draw_text(border, card, guest)
+        formatted_guest = guest.lower().replace(" ", "_").replace(".", "")
+        card.save(output_path / f'{formatted_guest}_card.png')
+
+
+def get_images(directory):
+    files = []
+    patterns = ('*.png', '*.jpg', '*.gif', '*.bmp')
+    for pattern in patterns:
+        files.extend(directory.glob(pattern))
+
+    return files
+
+
+def resize_image(filename):
+    image = Image.open(filename)
+    width, height = image.size
+    width_factor = width / 360
+    height_factor = height / 288
+    return image.resize((int(width / width_factor), int(height / height_factor)))
+
+
+def draw_text(image, text):
+    card_font = ImageFont.truetype('arial.ttf', 36)
+    width, height = image.size
+    text_width, text_height = get_text_size(image_size=image.size,
+                                            text=text,
+                                            font=card_font)
+
+    text_position = (int((width - text_width) / 2),
+                     int((height - text_height) / 2))
+
+    return draw_text_with_halo(image=image,
+                               position=text_position,
+                               text=text,
+                               font=card_font,
+                               halo_color='black',
+                               color='white')
+
+
+def get_text_size(image_size, text, font):
+    image = Image.new('RGBA', image_size, 'white')
+    draw = ImageDraw.Draw(image)
+    return draw.textsize(text=text, font=font)
+
+
+def draw_text_with_halo(image, position, text, font, halo_color, color):
+    halo = Image.new('RGBA', image.size, (0, 0, 0, 0))
+    ImageDraw.Draw(halo).text(position, text, font=font, fill=halo_color)
+    blurred_halo = halo.filter(ImageFilter.BLUR)
+    ImageDraw.Draw(blurred_halo).text(position, text=text, font=font, fill=color)
+    return Image.composite(image, blurred_halo, ImageChops.invert(blurred_halo))
+
+
+if __name__ == '__main__':
+    guest_path = Path('./guests.txt')
+    flower_dir = Path('./flower_images/')
+    make_seating_cards(guest_list_path=guest_path, directory=flower_dir)
