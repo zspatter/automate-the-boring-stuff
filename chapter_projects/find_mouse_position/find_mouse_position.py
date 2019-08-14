34c5cfe61e20fae81f5a98536823aa5039b2308a
zspatter <localhost@zspatter.dev>
2019-08-14 09:16:49 -0400

    adds RGB value of pixel at mouse position to program

--- chapter_projects/find_mouse_position/find_mouse_position.py
+++ chapter_projects/find_mouse_position/find_mouse_position.py
@@ -3,15 +3,15 @@ import pyautogui
 
 def get_mouse_position():
     print('Press Ctrl-C to quit.\n')
-    reset_line = '\033[2K\u001b[20D'
     backspace = '\b'
 
     try:
         while True:
             x, y = pyautogui.position()
-            position = f'X: {x:>6,d}\tY: {y:>5,d}'
+            rgb = pyautogui.screenshot().getpixel((x, y))
+            position = f'X: {x:>6,d}\tY: {y:>5,d}\t' \
+                       f'RGB: ({rgb[0]:>3d}, {rgb[1]:>3d}, {rgb[2]:>3d})'
             print(f'{backspace * len(position)}{position}', end='', flush=True)
-            # print(f'{reset_line}{position}', end='', flush=True)
 
     except KeyboardInterrupt:
         print('\nDone')
