f73a9aa928bc100351f40505f2e4e41647b9d882
zspatter <localhost@zspatter.dev>
2019-08-16 13:54:58 -0400

    refactors selective_copy to include a simpler solution and adds missing docstrings

--- selective_copy/selective_copy.py
+++ selective_copy/selective_copy.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python3
 # selective_copy.py - selectively copies a specific filetype's matched
-#                       files to a destination directory TODO generator?
+#                   files to a destination directory (renaming as needed)
 
 import os
 import re
 import shutil
+from pathlib import Path
 
 
 def selective_copy(source, destination_path, extension):
+    """
+    Selectively copies files of the extension's type from the source
+    path to the destination path renaming the files as needed (sequentially)
+
+    :param str source: path to directory to search (recursively)
+    :param str destination_path: path to output directory
+    :param str extension: desired filetype's extension
+    """
     directory = os.path.abspath(destination_path)
 
     # if destination doesn't exist, it's created
@@ -31,7 +40,7 @@ def selective_copy(source, destination_path, extension):
 
                 # if file of this name exists in destination
                 if os.path.exists(os.path.join(destination_path, filename)):
-                    regex = re.compile(f'(.*)(_\\d+)?({extension})')
+                    regex = re.compile(f'(.*?)(_\\d+)?({extension})')
 
                     increment = 1
                     check_name = os.path.join(directory,
@@ -53,5 +62,37 @@ def selective_copy(source, destination_path, extension):
                     f'\tCopied {os.path.basename(filepath)} to {os.path.abspath(destination_path)}')
 
 
+def simple_selective_copy(source, destination, extension):
+    """
+    Selectively copies files of the extension's type from the source
+    path to the destination path renaming the files as needed (sequentially)
+
+    :param Path source: path to directory to search (recursively)
+    :param Path destination: path to output directory
+    :param str extension: desired filetype's extension
+    """
+    matches = list(source.rglob(pattern=f'*{extension}'))
+    destination.mkdir(exist_ok=True)
+
+    for match in matches:
+        new_path = destination / match.name
+
+        if match.parent.resolve() == destination.resolve():
+            continue
+
+        if new_path.exists():
+            regex = re.compile(f'(.*?)(_\\d+)?({extension})', re.IGNORECASE)
+            regex_match = regex.search(new_path.name)
+            increment = int(regex_match.group(2)) + 1 if regex_match.group(2) else 1
+
+            while new_path.exists():
+                new_path = destination / f'{regex_match.group(1)}_{increment}{new_path.suffix}'
+                increment += 1
+
+        shutil.copy(src=f'{match}', dst=f'{new_path}')
+        print(f'Copied "{match}" to "{new_path}"')
+
+
 if __name__ == '__main__':
-    selective_copy(source='..', destination_path='./matches/', extension='.txt')
+    # selective_copy(source='.', destination_path='./matches/', extension='.txt')
+    simple_selective_copy(source=Path('.'), destination=Path('./matches/'), extension='.txt')
