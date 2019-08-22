156ae84828f27776c926cb83c2605eb6637a077f
zspatter <localhost@zspatter.dev>
2019-08-22 11:33:27 -0400

    adjusts the logic after breaking out of loop in `look_busy_until()` and `look_busy_for()` to log out of windows machines
    docstring function definitions have been added as well

--- looking_busy/looking_busy.py
+++ looking_busy/looking_busy.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 # looking_busy.py - simple script that simulates user input to keep IM status active
 
+import ctypes
 import random
 from datetime import datetime, time, timedelta
 from time import sleep
@@ -8,7 +9,16 @@ from time import sleep
 import pyautogui
 
 
-def simulated_input(interval):
+def simulate_input(interval):
+    """
+    Nudges the mouse by random x and y values (-10 to 10), then returns
+    the mouse to it's original position. The duration of these moves is
+    a random time value between 0.1 and 0.5 seconds.
+
+    A function key bound to no action is chosen at random to simulate keyboard input
+
+    :param float interval: max interval between mouse nudges
+    """
     x = random.randint(a=-10, b=10)
     y = random.randint(a=-10, b=10)
 
@@ -21,46 +31,61 @@ def simulated_input(interval):
 
 def look_busy(interval=60):
     """
-    Nudges the mouse by random x and y values (-10 to 10), then returns
-    the mouse to it's original position. The duration of these moves is
-    a random time value between 0.1 and 0.5 seconds.
-
-    A function key bound to no action is chosen at random to simulate keyboard input
+    Continues to simulate mouse/keyboard input until the process is
+    explicitly interupted
 
     :param float interval: max interval between mouse nudges
     """
-    print('Press CTRL-C to quit.\n')
     try:
+        print('Press CTRL-C to quit.\n')
+
         while True:
-            simulated_input(interval=interval)
+            simulate_input(interval=interval)
 
     except KeyboardInterrupt:
         print('Process quit.')
 
 
 def look_busy_until(interval=60, until=time(hour=10, minute=15)):
-    try:
-        while datetime.now().time() < until:
-            simulated_input(interval=interval)
+    """
+    Simulates mouse/keyboard input until the specified time
 
-        pyautogui.hotkey('win', 'l')
+    :param float interval: max interval between mouse nudges
+    :param datetime.time until: time to stop input
+    """
+    try:
+        print('Press CTRL-C to quit.\n')
+
+        while datetime.now().time() < until:
+            simulate_input(interval=interval)
+
+        ctypes.windll.user32.LockWorkStation()
+
     except KeyboardInterrupt:
         print('Process quit.')
 
 
 def look_busy_for(interval=60, duration=timedelta(hours=1, minutes=30)):
+    """
+    Simulates mouse/keyboard input for a given duration
+
+    :param float interval: max interval between mouse nudges
+    :param datetime.timedelta duration: duration to simulate input for
+    """
     end = datetime.now() + duration
 
     try:
+        print('Press CTRL-C to quit.\n')
         while datetime.now() < end:
-            simulated_input(interval=interval)
+            simulate_input(interval=interval)
 
-        pyautogui.hotkey('win', 'l')
+        ctypes.windll.user32.LockWorkStation()
+
     except KeyboardInterrupt:
         print('Process quit.')
 
 
 if __name__ == '__main__':
-    look_busy_until(until=time(hour=11))
-    # look_busy_for(duration=timedelta(minutes=5))
+    look_busy_until(interval=5 * 60, until=time(hour=10, minute=20))
+    # look_busy_for(interval=5 * 60, duration=timedelta(hours=1, minutes=30))
     # look_busy(interval=5 * 60)
