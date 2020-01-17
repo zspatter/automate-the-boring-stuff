33a3c4b5a1f5007ea0d78161561ddada88d71c04
zspatter <localhost@zspatter.dev>
2020-01-17 17:22:29 -0500

    Adds new edge test cases

--- regex_strip/test_regex_strip.py
+++ regex_strip/test_regex_strip.py
@@ -1,12 +1,20 @@
 from regex_strip import regex_strip
 
-whitespace = ['      test       ', '\nsample', '\ttext', '\n\twhitespace\n\t']
-custom = [('123test123', '123'), ('abcobfuscateabc', 'abc'), ('sampleEXAMPLEsample', 'sample')]
+default_cases = ['      test       ',
+                 '\nsample',
+                 '\ttext',
+                 '\n\twhitespace\n\t']
+
+custom_cases = [('123test123', '123'),
+                ('abcobfuscateabc', 'abc'),
+                ('sampleEXAMPLEsample', 'sample'),
+                ('123home123home123', '123'),
+                ('123123home123home123123', '123')]
 
 
 def test_regex_strip():
-    for text in whitespace:
+    for text in default_cases:
         assert regex_strip.regex_strip(text) == text.strip()
 
-    for text, remove in custom:
+    for text, remove in custom_cases:
         assert regex_strip.regex_strip(text, remove) == text.strip(remove)
